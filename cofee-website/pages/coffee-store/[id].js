933b7c85a5a041f92589ed06afc8b011dd9775e7
mrprg <mohamadrezaasadi322@gmail.com>
2023-01-12 20:12:34 +0330

    learning how to use router in Next

---
+++ cofee-website/pages/coffee-store/[id].js
@@ -0,0 +1,9 @@
+import { useRouter } from 'next/router'
+const CoffeeStore = () => {
+    const router = useRouter();
+    return (
+        <h1>Coffee Shop</h1>
+    )
+};
+
+export default CoffeeStore;
