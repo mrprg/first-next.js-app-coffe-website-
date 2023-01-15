2cee37c8cac6655b54a7dd8a4affaa17ed34dc84
mrprg <mohamadrezaasadi322@gmail.com>
2023-01-15 22:14:15 +0330

    add fonts localy to pages

--- cofee-website/pages/_app.js
+++ cofee-website/pages/_app.js
@@ -1,5 +1,31 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import localfont from '@next/font/local';
 
+const notoSerifToto = localfont({ 
+  src: [
+    {
+      path: '../public/fonts/NotoSerifToto-Bold.ttf',
+      weight: '700',
+      style: 'Bold'
+    },
+    {
+      path: '../public/fonts/NotoSerifToto-Regular.ttf',
+      weight: '400',
+      style: 'Regular'
+    },
+    {
+      path: '../public/fonts/NotoSerifToto-SemiBold.ttf',
+      weight: '600',
+      style: 'Semi-Bold'
+    }
+
+  ]
+
+})
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps}/> 
+  return( 
+  <main className={notoSerifToto.className}>
+    <Component {...pageProps}/>
+  </main>
+  )
 }
