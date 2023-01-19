8eaf363ca39bf6d97516c0b31a67035102b3815d
mrprg <mohamadrezaasadi322@gmail.com>
2023-01-19 19:49:27 +0330

    Card Component Structure

--- cofee-website/components/Card.js
+++ cofee-website/components/Card.js
@@ -1,9 +1,17 @@
+import Link from "next/link";
+import Image from "next/image";
 import styles from "../styles/Card.module.css";
-const Card = () => {
+const Card = (props) => {
   return (
-    <div className={styles.container}>
-        this is card component
-    </div>
+    <Link href={props.href}>
+
+        <h2>{props.name}</h2>
+        <Image
+          src={props.imgUrl}
+          width={260}
+          height={160}
+        />
+    </Link>
   );
 };
 
