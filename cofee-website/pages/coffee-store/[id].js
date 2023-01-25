2ce19e3ed6038f7f1dfc22918dcdb658711ed12d
mrprg <mohamadrezaasadi322@gmail.com>
2023-01-25 20:47:26 +0330

    impelementing getStaticPaths into dynamic page

--- cofee-website/pages/coffee-store/[id].js
+++ cofee-website/pages/coffee-store/[id].js
@@ -1,9 +1,30 @@
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
+import coffeeStoreData from "../../data/5.1 coffee-stores.json";
+
+export async function getStaticProps(staticProps) {
+  const params = staticProps.params;
+  return {
+    props: {
+      coffeeStore: coffeeStoresData.find((coffeeStore) => {
+        return coffeeStore.id === 0; //dynamic id
+      }),
+    },
+  };
+}
+
+export function getStaticPaths() {
+  return {
+    paths: [
+      { params: { id: "0" } },
+      { params: { id: "1" } },
+    ],
+  };
+}
+
 const CoffeeStore = () => {
-    const router = useRouter();
-    return (
-        <h1>Coffee Shop</h1>
-    )
+  const router = useRouter();
+  console.log(router);
+  return <h1>Coffee Shop {router.query.id}</h1>;
 };
 
 export default CoffeeStore;
